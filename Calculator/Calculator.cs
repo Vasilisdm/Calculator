29d0ff309dbaf47dc281ff25b4e1346f1b401de3
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-12 18:20:44 +0200

    Wrapped the dividebyzero exception to one more general, and in order not to lose any information, I added a message alongside with the inner(original) exception

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -13,9 +13,9 @@ namespace Calculator
                 {
                     return Divide(number1, number2); 
                 }
-                catch (DivideByZeroException)
+                catch (DivideByZeroException ex)
                 {
-                    throw;
+                    throw new ArithmeticException("An error occured during calculation.", ex);
                 }
             }
             else
